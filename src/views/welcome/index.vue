045922b532e2cd07b81c1f0f5fbd73f57bbcbcdb
zhousg <zhoushugang@itcast.cn>
2019-08-23 14:29:49 +0800

    首页基本完成

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='container' style="text-align:center">
+    <img src="../../assets/images/welcome.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
