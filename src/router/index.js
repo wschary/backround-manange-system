618e8a889f1cb72061edb286201a151e17aa7d5d
wszhang <zjh19930301@163.com>
2019-08-25 15:23:04 +0800

    主页面搭建完成及404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
@@ -11,7 +14,17 @@ const router = new VueRouter({
   routes: [
     // name: 'login' 给当前路由取名
     // 跳转使用：$router.push('/login') 或者 $router.push({name:'login'})
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎页面  如果子路由有名字  父级路由需要删除
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    },
+    // 404 处理  通配
+    { path: '*', name: '404', component: NotFound }
   ]
 })
 
