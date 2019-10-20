1069b2c4e730fc820bb2185796ccecfbe4cfb7c6
wszhang <zjh19930301@163.com>
2019-10-20 23:25:06 +0800

    login1

---
+++ src/views/zhuce/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='stylus'>
+
+</style>
